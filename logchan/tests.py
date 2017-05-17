2dc8bf7d58fe28393ca6563654cfd91179241d43
GF-101010 <GF-101010@gmx.com>
2017-05-17 13:29:57 +0200

    Add first test for Rest API

--- logchan/tests.py
+++ logchan/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from logchan.models import Board
+
+class RestApiTest(TestCase):
+    def test_post(self):
+        factory = APIClient()
+        boardName = 'Test board'
+        request = factory.post('/api/boards/', {'name': boardName}, format='json')
+        self.assertEqual(request.status_code, 201)
+        self.assertEqual(Board.objects.get(name=boardName).name, boardName)
+
+## Test view
+# https://github.com/erkarl/django-rest-framework-oauth2-provider-example/blob/master/apps/users/tests.py
