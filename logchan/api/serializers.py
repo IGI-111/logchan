45b8f2b2e8cd873905d90bce8f7e42c21e9f98fd
IGI-111 <igi-111@protonmail.com>
2017-05-22 11:24:18 +0200

    added form to thread view and moved to non hyperlinked serializers

--- logchan/api/serializers.py
+++ logchan/api/serializers.py
@@ -2,17 +2,17 @@ from rest_framework import serializers
 from ..models import Board, Thread, Post
 
 # Serializers define the API representation.
-class BoardSerializer(serializers.HyperlinkedModelSerializer):
+class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
         fields = ["name"]
 
-class ThreadSerializer(serializers.HyperlinkedModelSerializer):
+class ThreadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Thread
         fields = ["id", "board", "subject"]
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ["id", "thread", "date", "user_name", "image", "message"]
--- logchan/views.py
+++ logchan/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Board, Thread, Post
 
 def index(request):
