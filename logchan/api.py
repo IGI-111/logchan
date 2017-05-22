4e11789c4f1dab4e06314629e738ce1a59298607
IGI-111 <igi-111@protonmail.com>
2017-05-22 09:04:44 +0200

    added per board thread list

--- logchan/api.py
+++ logchan/api.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from .models import Board, Thread
-from rest_framework import routers, serializers, viewsets
+from rest_framework import serializers, viewsets
+from rest_framework_nested import routers
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 
@@ -24,14 +25,30 @@ class ThreadViewSet(viewsets.ModelViewSet):
     queryset = Thread.objects.all()
     serializer_class = ThreadSerializer
 
+class ThreadByBoardViewSet(ThreadViewSet):
+    def list(self, request, board_pk=None):
+        queryset = self.queryset.filter(board=board_pk)
+        serializer = ThreadSerializer(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
+
+    def retreive(self, request, board_pk=None):
+        queryset = self.queryset.filter(board=board_pk)
+        thread = get_object_or_404(queryset, board=board_pk)
+        serializer = ThreadSerializer(threads, many=True, context={'request':request})
+        return Response(serializer.data)
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'board', BoardViewSet)
 router.register(r'thread', ThreadViewSet)
 
+board_router = routers.NestedSimpleRouter(router, r'board', lookup='board')
+board_router.register(r'thread', ThreadByBoardViewSet, base_name='board-thread')
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^', include(board_router.urls)),
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
