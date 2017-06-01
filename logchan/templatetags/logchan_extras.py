8b5ddf01e450f8cbb02ccf983c0d2d32e8792051
IGI-111 <igi-111@protonmail.com>
2017-06-01 16:32:52 +0200

    fix for exception checking

--- logchan/templatetags/logchan_extras.py
+++ logchan/templatetags/logchan_extras.py
@@ -1,12 +1,13 @@
 from django import template
 from django.contrib.auth.models import Group
+from django.core.exceptions import ObjectDoesNotExist
 
-register = template.Library() 
+register = template.Library()
 
-@register.filter(name='is_in_group') 
+@register.filter(name='is_in_group')
 def is_in_group(user, group_name):
     try:
         group =  Group.objects.get(name=group_name)
         return group in user.groups.all()
-    except Answer.DoesNotExist:
-        return false
+    except ObjectDoesNotExist:
+        return False
