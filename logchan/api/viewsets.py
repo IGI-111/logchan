c502cfe07702354548cf7b458f70f6a8f9cd5cb9
IGI-111 <igi-111@protonmail.com>
2017-06-02 15:02:45 +0200

    fix module imports

--- logchan/api/viewsets.py
+++ logchan/api/viewsets.py
@@ -12,7 +12,27 @@ import requests
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from ..templatetags import logchan_extras
-from ..api import *
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def grecaptcha_verify(request):
+    data = request.POST
+    captcha_rs = data.get('g-recaptcha-response')
+    url = "https://www.google.com/recaptcha/api/siteverify"
+    params = {
+        'secret': settings.RECAPTCHA_SECRET_KEY,
+        'response': captcha_rs,
+        'remoteip': get_client_ip(request)
+    }
+    verify_rs = requests.get(url, params=params, verify=True)
+    verify_rs = verify_rs.json()
+    return verify_rs.get("success", False)
 
 # ViewSets define the view behavior.
 class BoardViewSet(viewsets.ModelViewSet):
